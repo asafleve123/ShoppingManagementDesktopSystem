2c860bb7b39ffb703f60c4c151ca873dee12c713
asafleve123@gmail.com <asafleve123@gmail.com>
2021-02-17 00:11:13 +0200

    Add project files.

---
+++ BE/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public uint price { get; set; }
+    }
+    
+}
